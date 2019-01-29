ca6f11f3f57bf2f4608aef0e12e478ff0de1c3ff
shorouq <shrooqabdullahsaad@gmail.com>
2019-01-29 16:23:39 +0200

    create router.js file

---
+++ src/router.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const {handelHomePage,handelPublicFiles,handelNotFound} = require('./handler');
+const router = (request, response) => {
+    const endPoint = request.url;
+    if (endPoint === '/') {
+
+        handelHomePage(request,response,endPoint);
+        
+    } else if (endPoint.indexOf('/public/') !== -1) {
+
+        handelPublicFiles(request,response,endPoint);
+        
+    } 
+    else {
+
+        handelNotFound(request,response);
+       
+    }
+}
+
+module.exports = router;
