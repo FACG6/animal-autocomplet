c2474fc20ebb34d329b8aba1e42865ca617de124
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-01-30 11:09:46 +0200

    route to animal file

--- src/router.js
+++ src/router.js
@@ -1,23 +1,25 @@
-const {handelHomePage,handelPublicFiles,handelNotFound,handelResultPage,handelSearchPage} = require('./handler');
+const { handelHomePage, handelPublicFiles, handelNotFound, handelResultPage, handelAnimalPage, handelSearchPage } = require('./handler');
 const router = (request, response) => {
     const endPoint = request.url;
     if (endPoint === '/') {
 
-        handelHomePage(request,response,endPoint);
-        
+        handelHomePage(request, response, endPoint);
+
     } else if (endPoint.indexOf('/public/') !== -1) {
 
-        handelPublicFiles(request,response,endPoint);
-        
-    } else if(endPoint === '/result'){
-        handelResultPage(request,response);
-    }else if(endPoint === '/search'){
-        handelSearchPage(request,response);
+        handelPublicFiles(request, response, endPoint);
+
+    } else if (endPoint === '/result') {
+        handelResultPage(request, response);
+    } else if (endPoint === '/search') {
+        handelSearchPage(request, response);
+    } else if (endPoint === '/animal') {
+        handelAnimalPage(request, response);
     }
     else {
 
-        handelNotFound(request,response);
-       
+        handelNotFound(request, response);
+
     }
 }
 
