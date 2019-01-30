a7db2f817d0624d40f08c2a45f4fcee6ea61323f
shorouq <shrooqabdullahsaad@gmail.com>
2019-01-30 15:27:06 +0200

    modify port to run on heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const router = require('./router.js');
 const server = http.createServer(router);
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`server is running in localhost:${port}`);
 });
