d66c051dce2661573d4661202a17171215f1bd67
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-01-30 08:56:20 +0200

    add inputs

--- public/js/Dom.js
+++ public/js/Dom.js
@@ -1,2 +1,7 @@
-const divResult = document.querySelector('div');
-xhrRequest('get',"../../src/animal.json",(res)=>console.log(Object.keys(res)[0]));
+const inputText=document.getElementById("add-input");
+// const url ='../../src/animal.json'
+inputText.addEventListener('keyup',(event)=>{
+   let result= xhrRequest("GET","/animal",(res)=>{
+    console.log(Object.keys(res));
+   }
+   )})
