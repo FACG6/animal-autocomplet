57fa61c998d2f9d74a4d49a8a51183cd39ce7700
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-01-30 12:48:59 +0200

    add list for form

--- public/js/Dom.js
+++ public/js/Dom.js
@@ -3,19 +3,31 @@ const form = document.querySelector(".search");
 const textInput = document.getElementById("add-input");
 const createImage = document.createElement('img');
 const createUl = document.createElement('ul');
-const createLi = document.createElement('li');
 const createDiv = document.createElement('div');
 
 textInput.addEventListener('keyup', () => {
-    // createDiv.appendChild();
+    form.appendChild(createDiv);
+    console.log("jamal");
+    createDiv.appendChild(createUl);
     const result = xhrRequestKeyUp((response) => {
-        const value = textInput.value;
+        const value =  textInput.value.toUpperCase();
+        console.log(value);
         const result = filterAutoComlete(response, value);
+        deleteChild(createUl);
+        result.forEach(element =>{
+            const createLi = document.createElement('li');
+            createUl.appendChild(createLi);
+            createLi.textContent=element;
+        })
+       
 
     })
 
 
 
 });
-
-console.log('sdbfhj')
+let deleteChild = (parent) => {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
