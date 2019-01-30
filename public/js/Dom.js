25676787d5b37f68d9c5c14108e8c48b3c347583
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-01-30 11:07:28 +0200

    add result of xhr

--- public/js/Dom.js
+++ public/js/Dom.js
@@ -1,19 +1,21 @@
+
 const form = document.querySelector(".search");
 const textInput = document.getElementById("add-input");
-
 const createImage = document.createElement('img');
 const createUl = document.createElement('ul');
 const createLi = document.createElement('li');
 const createDiv = document.createElement('div');
 
 textInput.addEventListener('keyup', () => {
-    form.appendChild(createDiv);
-    let textValue = textInput.value;
-    createDiv.textContent = '';
-    createDiv.appendChild(createUl)
-    createUl.appendChild(createLi)
-    
-    
+    // createDiv.appendChild();
+    const result = xhrRequestKeyUp((response) => {
+        const value = textInput.value;
+        const result = filterAutoComlete(response, value);
+
+    })
+
+
+
 });
 
 console.log('sdbfhj')
